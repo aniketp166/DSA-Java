54fe7651fe7a7b0b9a512f0422e93b67f145d561
Aniket Patil <aniketp166@gmail.com>
2023-10-08 15:49:08 +0530

    Array

--- Array.java
+++ Array.java
@@ -1,6 +1,6 @@
 public class Array {
     public static void main(String[] args) {
-        int[] arr = { 35, 12, 1, 10, 36, 1 };
+        int[] arr = { 35, 12, 1, 10, 36, 1, 4, 5, 6 };
 
         // Find max int in array
         int maxInt = arr[0];
@@ -23,22 +23,39 @@ public class Array {
         }
         System.out.println("SecondHighest " + SecondHighest);
 
-        /*
-         * int largest = arr[0], Secondlargest = -1;
-         * // reverse array
-         * int low = 0, high = arr.length - 1;
-         * 
-         * while (low < high) {
-         * int temp = arr[low];
-         * arr[low] = arr[high];
-         * arr[high] = temp;
-         * low++;
-         * high--;
-         * }
-         * for (int i = 0; i < arr.length; i++) {
-         * System.out.println(arr[i]);
-         * }
-         */
+        // reverse array
+        int low = 0, high = arr.length - 1;
+
+        while (low < high) {
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            low++;
+            high--;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println(" ");
+        // reverse array in groups
+        int[] arrSub = { 35, 12, 1, 10, 36, 1, 4, 5, 6, 1, 2 };
+        int k = 3;
+        for (int i = 0; i < arrSub.length; i = i + k) {
+            int lowIndex = i, highIndex = i + k - 1;
+            if (highIndex > arrSub.length - 1) {
+                highIndex = arrSub.length - 1;
+            }
+            while (lowIndex < highIndex) {
+                int temp = arrSub[lowIndex];
+                arrSub[lowIndex] = arrSub[highIndex];
+                arrSub[highIndex] = temp;
+                lowIndex++;
+                highIndex--;
+            }
+        }
+        for (int i = 0; i < arrSub.length; i++) {
+            System.out.print(arrSub[i] + " ");
+        }
 
     }
 }
