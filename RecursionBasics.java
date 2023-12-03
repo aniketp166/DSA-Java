eb9f32c121feaa44b1ffff1bcdec04b16231627f
Aniket Patil <aniketp166@gmail.com>
2023-12-03 20:44:31 +0530

    recursions basics

--- RecursionBasics.java
+++ RecursionBasics.java
@@ -20,11 +20,27 @@ public class RecursionBasics {
         }
     }
 
+    static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    static int Power(int a, int b) {
+        if (b == 0) {
+            return 1;
+        }
+        return a * Power(a, b - 1);
+    }
+
     public static void main(String[] args) {
         // Print(5);
         int n = 8;
-        for (int i = 0; i < n; i++) {
-            System.out.println(Fibonacci(i));
-        }
+        // for (int i = 0; i < n; i++) {
+        // System.out.println(Fibonacci(i));
+        // }
+
+        System.out.println(Power(3, 4));
     }
 }
